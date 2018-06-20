2880ac83fd7df5db99f4a7bf2c1158b540eabfc3
raaghda <33297148+raaghda@users.noreply.github.com>
2018-06-20 20:36:27 +0200

    Fix format of service-worker file

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,30 @@
 self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-  
-  event.respondWith(
-    
-    caches.open('cachedJobs').then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found response in cache:', response);
+    console.log('Handling fetch event for', event.request.url);
 
-          return response;
-        }
+    event.respondWith(
 
-        console.log('Fetching request from the network');
+        caches.open('cachedJobs').then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                if (response) {
+                    console.log('Found response in cache:', response);
 
-        return fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
+                    return response;
+                }
 
-          return networkResponse;
-        });
-      }).catch(function(error) {
-        
-        // Handles exceptions that arise from match() or fetch().
-        console.error('Error in fetch handler:', error);
+                console.log('Fetching request from the network');
 
-        throw error;
-      });
-    })
-  );
+                return fetch(event.request).then(function(networkResponse) {
+                    cache.put(event.request, networkResponse.clone());
+
+                    return networkResponse;
+                });
+            }).catch(function(error) {
+
+                // Handles exceptions that arise from match() or fetch().
+                console.error('Error in fetch handler:', error);
+
+                throw error;
+            });
+        })
+    );
 });
